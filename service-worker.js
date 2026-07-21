07bca2925539a5d3ea392b8647db5230a2e05be5
ykkthezipper <30307601+ykkthezipper@users.noreply.github.com>
2026-07-21 08:04:04 +0800

    Add Grounds early pilot prompts
    Add Truck Prep, distributing linens and grounds bin tasks. Keep unknown hazards and controls blank until the worker completes and reviews them.

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "sam-mate-github-v10";
+const CACHE_NAME = "sam-mate-github-v11";
 const APP_FILES = ["./", "./index.html", "./tools.html", "./app-icon-180.png", "./app-icon-192.png", "./app-icon-512.png"];
 
 self.addEventListener("install", (event) => {
