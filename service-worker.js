b95f2eeaaab0daf6e7a152aa81e96d28fd8839da
ykkthezipper <30307601+ykkthezipper@users.noreply.github.com>
2026-07-21 14:52:08 +0800

    Refresh public app cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "sam-mate-github-v12";
+const CACHE_NAME = "sam-mate-github-v13";
 const APP_FILES = ["./", "./index.html", "./tools.html", "./app-icon-180.png", "./app-icon-192.png", "./app-icon-512.png"];
 
 self.addEventListener("install", (event) => {
