7ca610fa90176bd492f9d01549fab526979b667a
ykkthezipper <30307601+ykkthezipper@users.noreply.github.com>
2026-07-21 22:33:01 +0800

    Reuse the Microsoft Forms tab
    Use one named Forms tab for all three SAM workflows and refresh the public cache.

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "sam-mate-github-v14";
+const CACHE_NAME = "sam-mate-github-v15";
 const APP_FILES = ["./", "./index.html", "./vision.html", "./tools.html", "./app-icon-180.png", "./app-icon-192.png", "./app-icon-512.png"];
 
 self.addEventListener("install", (event) => {
