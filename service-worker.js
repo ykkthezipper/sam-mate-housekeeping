202a68351de29141b455dc6f43b0b009dfeaa34a
ykkthezipper <30307601+ykkthezipper@users.noreply.github.com>
2026-07-29 12:05:45 +0800

    Improve readability for older mobile users

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "sam-mate-github-v18-public-without-vision";
+const CACHE_NAME = "sam-mate-github-v19-readable-ui";
 const APP_FILES = ["./", "./index.html", "./tools.html", "./app-icon-180.png", "./app-icon-192.png", "./app-icon-512.png"];
 
 self.addEventListener("install", (event) => {
