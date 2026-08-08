fe7dedc13552e78a5047ee4ee40f7c45fa2255d4
ykkthezipper <30307601+ykkthezipper@users.noreply.github.com>
2026-08-08 21:44:09 +0800

    Publish updated public GitHub Pages build
    Updates the public build with the inline Personalise copy format panel and the revised Daily Tools order. Refrigeration remains excluded from the public build.

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "sam-mate-github-v20-text-size-toggle";
+const CACHE_NAME = "sam-mate-github-v21-department-shell";
 const APP_FILES = ["./", "./index.html", "./tools.html", "./app-icon-180.png", "./app-icon-192.png", "./app-icon-512.png"];
 
 self.addEventListener("install", (event) => {
