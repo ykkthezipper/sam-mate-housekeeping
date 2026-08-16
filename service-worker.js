8dc6da756679af68432799caeec4d1701e1d2ed3
ykkthezipper <30307601+ykkthezipper@users.noreply.github.com>
2026-08-17 06:44:13 +0800

    Refresh offline cache for Catering pilot

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const CACHE_NAME = "sam-mate-github-v23-copy-task-first";
-const APP_FILES = ["./", "./index.html", "./tools.html", "./app-icon-180.png", "./app-icon-192.png", "./app-icon-512.png"];
+const CACHE_PREFIX = "sam-mate:housekeeping-pilot";
+const CACHE_NAME = `${CACHE_PREFIX}:v3`;
+const APP_FILES = ["./", "./index.html", "./tools.html", "./room-extractor.html", "./app-icon-180.png", "./app-icon-192.png", "./app-icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
@@ -12,7 +13,7 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches
       .keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then((keys) => Promise.all(keys.filter((key) => key.startsWith(`${CACHE_PREFIX}:`) && key !== CACHE_NAME).map((key) => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
